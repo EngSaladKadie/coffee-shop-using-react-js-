b4be2ada6612e009ecfcbe311b62d3c73b7a5e2e
zalad <Mohaa9>
2025-05-11 00:18:21 +0300

    first commit

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReviewsCard from "../layouts/ReviewsCard";
-import img2 from "../assets/23.jpeg";
-import img3 from "../assets/13.jpeg";
-import img5 from "../assets/14.jpeg";
+import img2 from "../assets/c1.jpeg";
+import img3 from "../assets/c2.jpeg";
+import img5 from "../assets/c3.jpeg";
 
 const Reviews = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center lg:px-32 px-5 bg-backgroundColor">
-      <h1 className="font-semibold text-center lg:flex-row gap-5 justify-center py-4 my-8">
+      <h1 className="font-semibold text-center text-4xl lg:mt-14 mt-24">
         Customers Reviews
       </h1>
-      <div>
+      <div className="flex flex-col lg:flex-row gap-5 justify-center py-4 my-8">
         <ReviewsCard img={img2} title="zuu" />
         <ReviewsCard img={img3} title="zalad" />
         <ReviewsCard img={img5} title="ali" />
--- src/layouts/ReviewsCard.jsx
+++ src/layouts/ReviewsCard.jsx
@@ -6,7 +6,7 @@ const ReviewsCard = (props) => {
     <div className="flex flex-col w-full lg:w-2/6 bg-white p-3 rounded-lg gap-5">
       <div className="flex flex-row items-center lg:justify-start justify-center">
         <div className="w-1/4">
-          <img src={props.img} alt="" />
+          <img className="rounded-full" src={props.img} alt="" />
         </div>
         <div className="mx-3">
           <h2 className="font-semibold text-lg">{props.title}</h2>
