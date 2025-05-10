07e5d66a105bb10e286de54593c7c1ebb3dc26fb
zalad <Mohaa9>
2025-05-10 21:39:49 +0300

    first commit

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,12 +4,18 @@ import img from "../assets/1.jpeg";
 
 const Menu = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-between lg:px-32 px-5 bg-backgroundColor">
+    <div className="min-h-screen flex flex-col justify-center lg:px-32 px-5 bg-backgroundColor">
       <h1 className="font-semibold text-center text-4xl mt-24 mb-8">
         Our Menu
       </h1>
       <div className="flex flex-col justify-between">
-        <MenuCard  />
+        <MenuCard img={img} title="Espreesso"/>
+        <MenuCard img={img} title="Espreesso"/>
+        <MenuCard img={img} title="Espreesso"/>
+        <MenuCard img={img} title="Espreesso"/>
+        <MenuCard img={img} title="Espreesso"/>
+        <MenuCard img={img} title="Espreesso"/>
+        
         
 
       </div>
--- src/layouts/MenuCard.jsx
+++ src/layouts/MenuCard.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-import img from "../assets/1.jpeg";
+import img from "../assets/4.jpeg";
 import { FaShoppingCart } from "react-icons/fa";
 
 function MenuCard(props) {
   return (
-    <div className="w-full lg:h-1/4 bg-white p-2 rounded-lg">
+    <div className="w-full lg:w-1/4 bg-white p-3 rounded-lg">
       <div>
-        <img className="rounded-lg" src={img} alt="images" />
+        <img className="rounded-xl" src={img} alt="images" />
       </div>
       <div className="p-2 mt-5">
-        <div className="flex flex-col justify-between"> 
+        <div className="flex flex-row justify-between">
           <h3 className="font-semibold text-xl">{props.title}</h3>
           <h3 className="font-semibold text-xl">{props.value}</h3>
         </div>
-        <div>
-          <div>
-            <button>Hot</button>
-            <button>Cold</button>
+        <div className="flex flex-row justify-between mt-3">
+          <div className="flex gap-2">
+            <button className="p-3 text-sm border-2 border-brightColor bg-backgroundColor hover:text-brightColor transition-all rounded">
+              Hot
+            </button>
+            <button className="p-3 text-sm border-2 border-brightColor bg-backgroundColor hover:text-brightColor transition-all rounded">
+              Cold
+            </button>
           </div>
-          <span>
+          <span className="flex items-center bg-backgroundColor px-3 py-2 rounded-full cursor-pointer">
             <FaShoppingCart size={30} />
           </span>
         </div>
